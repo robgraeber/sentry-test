e7e558a8642a4e4d2295a0989b0c40590b9134f0
Rob Graeber <rgraeber@noreply.com>
2023-01-15 16:22:17 -0800

    initial commit

---
+++ packages/my-app/App.tsx
@@ -0,0 +1,27 @@
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'akjkjh',
+  enableInExpoDevelopment: true,
+  debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
